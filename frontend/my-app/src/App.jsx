156f101547bda767e6883a8db430936b8b26f454
Rokika Kh <rokika.kh1@gmail.com>
2025-02-22 21:36:54 +1100

    added styling and made responsive

--- frontend/my-app/src/App.jsx
+++ frontend/my-app/src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
 import Header from './components/header'
+import Nav from './components/Nav'
+import Buildings from './components/Buildings'
 
 function App() {
 
   return (
     <>
       <Header></Header>
+      <Nav></Nav>
+      <Buildings></Buildings>
     </>
   )
 }
--- frontend/my-app/src/components/Header.jsx
+++ frontend/my-app/src/components/Header.jsx
@@ -1,65 +1,27 @@
 import { useState } from "react";
 import freeRoomsLogo from "../assets/freeRoomsLogo.png";
 import freeRoomsLogoClosed from "../assets/freeroomsDoorClosed.png";
+import "./Header.css";
 
 function Header() {
     const [doorOpen, setDoorOpen] = useState(true);
 
-    const headerStyle = {
-        justifyContent: 'space-between',
-        display: 'flex',
-        alignItems: 'center',
-        padding: '10px 20px',
-        borderBottom: '0.5px solid grey'
-    };
-
-    const logoStyle = {
-        display: 'flex',
-        alignItems: 'center',
-    }
-
-    const logoImgStyle = {
-        width: '80px',
-        cursor: 'pointer',
-    };
-
-    const logoNameStyle = {
-        fontFamily: 'Roboto',
-        fontSize: '50px',
-        color: "#ea6d00",
-        margin: 0,
-    };
-
-    const iconsContainerStyle = {
-        display: 'flex',
-        gap: '15px',
-    };
-
-    const iconStlye = {
-        fontSize: "40px",
-        color: "#ea6d00",
-        padding: '10px',
-        border: '1px solid #ea6d00',
-        borderRadius: '5px',
-        cursor: 'pointer',
-    }
-
     return (
-        <div style={headerStyle}>
-            <div style={logoStyle}>
+        <div className="header">
+            <div className="logo">
                 <img
-                    style={logoImgStyle}
+                    className="logo-img"
                     src={doorOpen ? freeRoomsLogo : freeRoomsLogoClosed}
                     alt="Freerooms_Logo"
                     onClick={() => setDoorOpen(!doorOpen)}
                 />
-                <h1 style={logoNameStyle}>Freerooms</h1>
+                <h1 className="logo-name">Freerooms</h1>
             </div>
-            <div style={iconsContainerStyle}>
-                <span className="material-icons" style={iconStlye}>search</span>
-                <span className="material-icons" style={iconStlye}>grid_view</span>
-                <span className="material-icons" style={iconStlye}>map</span>
-                <span className="material-icons" style={iconStlye}>dark_mode</span>
+            <div className="icons-container">
+                <span className="material-icons icon">search</span>
+                <span className="material-icons icon icon-select">grid_view</span>
+                <span className="material-icons icon">map</span>
+                <span className="material-icons icon">dark_mode</span>
             </div>
         </div>
     );
